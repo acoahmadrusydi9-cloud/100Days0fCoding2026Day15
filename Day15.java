099062a5c544e815b7db0cbe575e6425ea4140d3
Aco Ahmad Rusydi R <acoahmadrusydi9@gmail.com>
2026-09-16 10:32:12 +0800

    Implement multiplication and division operations
    yeaahh

---
+++ Day15.java
@@ -0,0 +1,15 @@
+public class Day15 {
+    public static void main(String[] args) {
+        
+        double angka1 = 31;
+        double angka2 = 4;
+
+        //operasi aritmatika perkalian
+        double hasilPerkalian = angka1 * angka2;
+        System.out.println("Hasil perkalian: " + hasilPerkalian);
+
+        //operasi aritmatika pembagian
+        double operasiPembagian = angka1 / angka2;
+        System.out.println("Hasil pembagian: " + operasiPembagian);
+    }
+}
